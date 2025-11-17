2c8a19b1dcb4e3d8ed2c5136fff65b78bef96c92
Rafael0896 <realvarez54@misena.edu.co>
2025-11-16 20:35:18 -0500

    Update project structure: modified App.jsx and index.css; added Header component; removed style.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
+import Header from './components/Header.jsx';
+
 
 
 // Componente principal de la aplicación
 export default function App (){
     return(
         <div className="app">
-
+            <Header/>
         </div>
     )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const Header = () => (
+    <header className="header">
+        <div className="container">
+            <div className="header-content">
+                <div>
+                    <h1>Informe de Resultados - Comportamiento del Software</h1>
+                    <p className="header-subtitle">Evaluación de Calidad basada en estándares internacionales</p>
+                </div>
+                <div className="sena-logo">SENA</div>
+            </div>
+        </div>
+    </header>
+);
+
+export default Header;
